8bdd0da6b62cb58222189be1417d77ac9403bd9c
Ordinateur 1 <abcvika3@gmail.com>
2026-03-05 23:26:37 +0100

    add videomodal

---
+++ js/modal-video.js
@@ -0,0 +1,6 @@
+const wrapperVideo = document.querySelector(".making-process-video-wrapper");
+const modalVideo = document.querySelector("#modalMakingVideo");
+wrapperVideo.addEventListener("click", () => {
+  modalVideo.classList.toggle("is-open");
+});
+console.log(wrapperVideo);
